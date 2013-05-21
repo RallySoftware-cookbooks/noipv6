652df57d4434ca39a5267a3df829e5c0a8086266
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-05-21 13:28:31 -0600

    Adding a spec test for noipv6 to test creation of the system network file from a template

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,7 +2,13 @@ require 'chefspec'
 
 describe 'noipv6::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'noipv6::default' }
-  it 'should do something' do
-    pending 'Your recipe examples go here.'
+  it 'checks for the creation of the system network file from a Chef template' do
+    expect(chef_run).to create_file '/etc/sysconfig/network'
+    file = chef_run.template('/etc/sysconfig/network')
+    expect(file).to be_owned_by('root', 'root')
+    expect(file.mode).to eq("0644")
   end
+# it 'checks for correct disabled ipv6 syntax in the system network file' do
+#   expect(chef_run).to create_file_with_content '/etc/sysconfig/network', 'NETWORKING_IPV6=no'
+# end
 end
