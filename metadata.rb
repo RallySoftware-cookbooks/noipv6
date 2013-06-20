1669c8bfffda48678e90c55d1c54f27cb47b5223
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-06-19 22:48:17 -0600

    converted from double to single quotes, fixed maintainer name/email address/license

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             "noipv6"
-maintainer       "Tehmasp Chaudhri"
-maintainer_email "tchaudhri@rallydev.com"
-license          "Apache 2.0"
-description      "Installs/Configures noipv6"
+name             'noipv6'
+maintainer       'Rally Software Development Corp'
+maintainer_email 'rallysoftware-cookbooks@rallydev.com'
+license          'MIT'
+description      'Installs/Configures noipv6'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0"
+version          '1.0'
 
-recipe           "noipv6", "Installs/Configures noipv6"
+recipe           'noipv6', 'Installs/Configures noipv6'
 
 %w{ centos fedora oracle debian ubuntu }.each do |os|
   supports os
