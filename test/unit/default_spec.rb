91141ceac9884ef069d0bff3af152d7ec1bc77fb
Matt Farrar <mfarrar@rallydev.com>
2013-11-22 15:40:31 -0700

    Upgrade to Chefspec3

--- test/unit/default_spec.rb
+++ test/unit/default_spec.rb
@@ -1,19 +1,6 @@
 require_relative 'spec_helper'
 
 describe 'noipv6::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'noipv6::default' }
-  let (:file) { chef_run.template('/etc/sysconfig/network') }
-
-  it 'checks for the creation of the system network file from a Chef template' do
-    expect(chef_run).to create_file('/etc/sysconfig/network')
-  end
-
-  it 'ensures correct ownership applied to file' do
-    expect(file).to be_owned_by('root', 'root')
-  end
-
-  it 'ensures correct permissions applied to file' do
-    expect(file.mode).to eq('0644')
-  end
-
+  subject(:chef_run) { ChefSpec::Runner.new.converge described_recipe }
+  it { should create_template('/etc/sysconfig/network').with(owner: 'root', group: 'root', mode: '0644') }
 end
